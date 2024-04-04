f8053107ea1268d813149711244a36cd943242bf
JulieKodehode <julie.kodehode@gmail.com>
2024-04-04 11:16:47 +0200

    First commit

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log('Hello!');
+
+
+// HENT
+const listName = document.querySelector("#listName");
+console.log(listName);
+
+// LAG
+const liHjem = document.createElement("li");
+console.log(liHjem);
+
+const liProsjekter = document.createElement("li");
+
+const liKontakt = document.createElement("li");
+
+// MODIFISER
+liHjem.textContent = "Hjem";
+
+liProsjekter.textContent = "Prosjekter";
+
+liKontakt.textContent = "Kontakt"
+
+// SEND
+listName.appendChild(liHjem);
+
+listName.appendChild(liProsjekter); 
+
+listName.appendChild(liKontakt);
